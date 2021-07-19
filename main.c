1cc944a132e1c4934623e8eaaf7588790daa0a21
nandozanutto <nando.zanutto@gmail.com>
2021-07-19 13:18:57 -0300

    graph+input done

--- main.c
+++ main.c
@@ -13,21 +13,28 @@ struct Transactions
     int id;
     char op;
     char attri;
+    apontTrans next;//ponteiro para a próx transação Ti
 };
 
 struct Agendamento
 {
-    apontTrans *transacao;//vetor de apontadores para transações
+    apontTrans *vetTransacao;//vetor de apontadores para transações
     int tam;//quantidade de transações
-    int primeiraVazia; //primeira pos vazia no vetor
 };
 
-apontAgen criaAgendamento(int tam){
+apontAgen criaAgendamento(){
     apontAgen agendamento = (apontAgen) malloc(sizeof(struct Agendamento));
-    agendamento->transacao = malloc(tam*sizeof(apontTrans));
-    agendamento->tam = tam;
-    agendamento->primeiraVazia = 0;
+    agendamento->tam = 0;
+    agendamento->vetTransacao=NULL;
     return agendamento;
+}
+int retornaPos(apontAgen agendamento, char id){
+    //retorna -1 se não há nenhuma transação com o mesmo id. Caso contrário retorna a pos
+    //da transição com o mesmo id
+    for(int i=0; i<agendamento->tam; i++)
+        if(agendamento->vetTransacao[i]->id == id)
+            return i;
+    return -1;
 }
 
 void criaTransacao(apontAgen agendamento, int time, int id, char op, char attri){
@@ -36,67 +43,91 @@ void criaTransacao(apontAgen agendamento, int time, int id, char op, char attri)
     transacao->op = op;
     transacao->id = id;
     transacao->time = time;
-    agendamento->transacao[agendamento->primeiraVazia] = transacao;
-    agendamento->primeiraVazia++;
+    transacao->next = NULL;
+    
+    int pos = retornaPos(agendamento, id);
+    if(pos!=-1){
+        apontTrans aux = agendamento->vetTransacao[pos];
+        while(aux->next != NULL)
+            aux = aux->next;
+        aux->next = (apontTrans) transacao;
+    }else{//nenhuma transação com o mesmo id
+        agendamento->vetTransacao = (apontTrans *) realloc(agendamento->vetTransacao, sizeof(apontTrans)*(agendamento->tam+1));//aumenta vetor
+        agendamento->vetTransacao[agendamento->tam] = transacao;
+        agendamento->tam++;
+    }
+    
+
 
 }
 
-
-
-
-// void printAgen(Agendamento agen[]){
-//     for(int j=1; j<2; j++){
-//         for(int i=0; i<agen[j].tam; i++)
-//             printf("%d %d %c %c\n", agen[j].trans[i].time, agen[j].trans[i].id, agen[j].trans[i].op, agen[j].trans[i].attri);
-//     }
-// }
+void testSeriab(apontAgen agendamento){
+    for(int j=0; j<agendamento->tam; j++){
+        for(apontTrans transacaoTj=agendamento->vetTransacao[j]; transacaoTj!=NULL; transacaoTj=transacaoTj->next){
+            if(transacaoTj->op == 'R'){
+                for(int i=0; i<agendamento->tam; i++){//varre as transações a procura de um write 
+                    if(i==j) continue;//se Ti está comparando com Ti
+                    for(apontTrans transacaoTi=agendamento->vetTransacao[i]; transacaoTi!=NULL; transacaoTi=transacaoTi->next){
+                        if((transacaoTi->op=='W') && (transacaoTj->attri==transacaoTi->attri) && (transacaoTj->time>transacaoTi->time))
+                            printf("Aresta R,W em %d -> %d\n", transacaoTi->id, transacaoTj->id);
+                    }
+                }
+            }else if(transacaoTj->op == 'W'){
+                for(int i=0; i<agendamento->tam; i++){//varre as transações a procura de um write 
+                    if(i==j) continue;//se Ti está comparando com Ti
+                    for(apontTrans transacaoTi=agendamento->vetTransacao[i]; transacaoTi!=NULL; transacaoTi=transacaoTi->next){
+                        if((transacaoTi->op=='R') && (transacaoTj->attri==transacaoTi->attri) && (transacaoTj->time>transacaoTi->time))
+                            printf("Aresta W,R em %d -> %d\n", transacaoTi->id, transacaoTj->id);
+                        if((transacaoTi->op=='W') && (transacaoTj->attri==transacaoTi->attri) && (transacaoTj->time>transacaoTi->time))
+                            printf("Aresta W,W em %d -> %d\n", transacaoTi->id, transacaoTj->id);
+                    }
+                }
+            }
+        }
+    }
+}
 
 
 int main(){
 
-    apontAgen agendamento = criaAgendamento(5);
-    criaTransacao(agendamento, 1, 2, 'W', 'X');
-    criaTransacao(agendamento, 2, 3, 'R', 'X');
-    printf("%c", agendamento->transacao[1]->op);
-    
-    
-    // Transaction trans;
-    // Agendamento agen[100];
-    // agen[0].tam = 0;
-    // agen[1].tam= 0;
-    // int time, id, commit=0;
-    // char op, attri;
-    // int i=0;
-    // int agenI=0;
-    // int somado=0;
-    // while(1){
-    //     int time;
-    //     int id;
-    //     char op;
-    //     char attri;
-    //     if(scanf("%d", &time) == EOF)
-    //         break;
-    //     scanf("%d %c %c", &id, &op, &attri);
-    //     if(op == 'C'){
-    //         if(!somado){
-    //             agenI++;
-    //             somado=1;
-    //         }
-    //         i=0;
-    //         continue;
-    //     }
-    //     else{
-    //         agen[agenI].trans[i].time = time;
-    //         agen[agenI].trans[i].id = id;
-    //         agen[agenI].trans[i].op = op;
-    //         agen[agenI].trans[i].attri = attri;
-    //     }
-    //     somado=0;
-    //     i++;
-    //     agen[agenI].tam++;
+    // apontAgen agendamento = criaAgendamento();
+    // criaTransacao(agendamento, 1, 1, 'R', 'X');
+    // criaTransacao(agendamento, 2, 2, 'R', 'X');
+    // criaTransacao(agendamento, 3, 3, 'R', 'X');
+    // criaTransacao(agendamento, 4, 1, 'R', 'X');
+    // criaTransacao(agendamento, 5, 1, 'R', 'X');
+    // criaTransacao(agendamento, 6, 3, 'R', 'X');
+    // criaTransacao(agendamento, 7, 2, 'R', 'X');
 
+
+    
+    apontAgen agendamento = criaAgendamento();
+    int commits=0;
+    while(1){
+        int time;
+        int id;
+        char op;
+        char attri;
+        if(scanf("%d", &time) == EOF)//checa se existe nova linha
+            break;
+        scanf("%d %c %c", &id, &op, &attri);
+        if(op == 'C'){
+            commits++;//commits não são salvos
+            if(commits == agendamento->tam){//supõe que os commits são dados em seq
+                commits=0;
+                break;
+            }
+        }
+        else{
+                criaTransacao(agendamento, time, id, op, attri);
+        }
  
-    // }
-    // printAgen(agen);
+    }
 
+    for(int i=0; i<agendamento->tam; i++){
+        for(apontTrans j=agendamento->vetTransacao[i]; j!=NULL; j=j->next)
+            printf(".%d %d %c %c. ", j->time, j->id, j->op, j->attri);
+        printf("\n");
+    }
+    testSeriab(agendamento);
 }
